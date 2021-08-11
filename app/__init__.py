95385cbcd835c4565b244818d3f6f3e741c78b9b
dgosoriog <46572605+dgosoriog@users.noreply.github.com>
2021-08-11 18:52:42 -0500

    final 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, redirect, url_for, session
+from flask_admin.contrib.sqla import ModelView
 from flask_admin.menu import MenuLink
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_admin import Admin, AdminIndexView, expose
 from flask_babelex import Babel
-from flask_security import current_user
+from flask_security import current_user, Security
 from flask_mail import Mail
+from flask_login import LoginManager
+
+from flask_principal import Principal, Identity, AnonymousIdentity, \
+    identity_changed, identity_loaded, UserNeed, RoleNeed, Permission
 import os
 #from app.config import Config
 #mail = Mail()
@@ -14,46 +19,55 @@ import os
 
 app = Flask(__name__)
 #app.config.from_object(Config)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///basereco.db'
+app.config['SECRET_KEY'] = b'3\x0f\x85\xb6\xf8M4P\x8e\xedA\xb8\xf0\xd5#\t'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baseaf.db'
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-app.config['SECURITY_PASSWORD_SALT'] = 'abcdefg'
-app.config['SECURITY_LOGIN_USER_TEMPLATE'] = 'templates/login.html'
+#app.config['SECURITY_PASSWORD_SALT'] = 'MDfy8APT'
+#app.config['SECURITY_LOGIN_USER_TEMPLATE'] = 'login.html'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME']='oso95d@gmail.com'
-app.config['MAIL_PASSWORD']='myfirstaccount'
-app.config['SESSION_PROTECTION'] = 'strong'
+app.config['MAIL_USERNAME']='floricolaasistente5@gmail.com'
+app.config['MAIL_PASSWORD']='MO2021818'
+app.config['AF_ADMIN'] ='oso95d@gmail.com'
 mail = Mail(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-class MyAdminIndexView(AdminIndexView):
-    @expose('/')
-    def home_admin(self):
-        return self.render('admin/index.html')
-
-    def is_accessible(self):
-        return current_user.has_role('admin')
-
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login'))
-
-admin = Admin(app,index_view=MyAdminIndexView())
-admin.name = "Recomendador"
-admin.template_mode = "bootstrap4"
-admin.add_link(MenuLink(name='Salir', url='/logout'))
-babel = Babel(app)
-@babel.localeselector
-def get_locale():
-     override = 'es'
-     if override:
-         session['lang'] = override
-     return session.get('lang', 'en')
-
+login = LoginManager(app)
+# class AdminView(ModelView):
+#     #@expose('/')
+#     def is_accessible(self):
+#         return current_user.has_role('admin')
+#
+#     def inaccessible_callback(self, name, **kwargs):
+#         return redirect(url_for('login'))
+#
+# class MyAdminIndexView(AdminIndexView):
+#     @expose('/')
+#     def home_admin(self):
+#          return self.render('admin/index.html')
+#
+#     def is_accessible(self):
+#         return current_user.has_role('admin')
+#
+#     def inaccessible_callback(self, name, **kwargs):
+#         return redirect(url_for('login'))
+#
+# admin = Admin(app,'Admin', url='/home',index_view=MyAdminIndexView())
+# admin.name = "Recomendador"
+# admin.template_mode = "bootstrap4"
+# admin.add_link(MenuLink(name='Salir', url='/logout'))
+# babel = Babel(app)
+# @babel.localeselector
+# def get_locale():
+#      override = 'es'
+#      if override:
+#          session['lang'] = override
+#      return session.get('lang', 'en')
+from app.errors.handlers import errors
+app.register_blueprint(errors)
 
 from app import routes
-
 #mail.init_app(app)
 #db.init_app(app)
 
